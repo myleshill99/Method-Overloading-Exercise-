b67bd37681b11856de3a972e976b1a9ccc8da9ee
Myles Hill <mylehill99@gmail.com>
2023-11-02 11:30:31 -0400

    finished

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = Methods.Add(2, 1);
+            Console.WriteLine(answer);
+
+            var answer2 = Methods.Add(2.0, 1.0);
+            Console.WriteLine(answer2);
+
+            var answer3 = Methods.Add(2.0m, 2.1m);
+            Console.WriteLine(answer3);
+
+            var answer4 = Methods.Add(2, 1, true);
+            Console.WriteLine(answer4);
+        }
+
+
+    }
+
+    internal class Methods
+    {
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool withDollars)
+        {
+            if (withDollars && (x + y) != 1)
+            {
+                return $"{x + y} dollars";
+            }
+            else
+            {
+                return $"{x + y}";
+            }
         }
     }
 }
